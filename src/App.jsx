809afa928e6111b04685233f2480baf123c82096
demasiadowear <info@demasiadowear.com>
2025-07-15 14:45:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ContentProvider } from './contexts/ContentContext';
+import { AuthProvider } from './contexts/AuthContext'; // NUOVO
+import ProtectedRoute from './components/ui/ProtectedRoute'; // NUOVO
 import Home from './components/Home';
-import AdminPanel from './components/AdminPanel';
-import './App.css';
+import AdminPanel from './components/ui/AdminPanel';
 
 function App() {
   return (
-    <ContentProvider>
-      <Router>
-        <div className="min-h-screen bg-background">
+    <AuthProvider> {/* NUOVO WRAPPER */}
+      <ContentProvider>
+        <Router>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/admin" element={<AdminPanel />} />
+            <Route 
+              path="/admin" 
+              element={
+                <ProtectedRoute> {/* PROTEZIONE */}
+                  <AdminPanel />
+                </ProtectedRoute>
+              } 
+            />
           </Routes>
-        </div>
-      </Router>
-    </ContentProvider>
+        </Router>
+      </ContentProvider>
+    </AuthProvider>
   );
 }
 
 export default App;
-
